2782e890287c78005bebf8c6eddc4aea38837bc3
Adipati Arya <mac@MACs-MacBook-Pro.local>
2019-07-07 18:39:10 +0700

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+  
   const div = document.createElement('div');
+  
   ReactDOM.render(<App />, div);
+  
+  expect(div.innerHTML).toContain('Hi there!');
+  
   ReactDOM.unmountComponentAtNode(div);
+
 });
